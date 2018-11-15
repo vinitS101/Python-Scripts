fd2812f3c47da411f5f941d6a32f39609c0d04a3
Vinit Singh <vinit.singh9013@gmail.com>
2018-11-15 15:11:55 +0530

    Auto Login Script

--- bitPlacementPortal.py
+++ bitPlacementPortal.py
@@ -1,7 +1,5 @@
 #A script to automatically login to the BIT Placement portal.
 #Can be used as a reference to create auto login scripts for other websites
-
-
 import webbrowser, time
 from selenium import webdriver
 
@@ -9,17 +7,9 @@ browser = webdriver.Firefox()
 browser.get('http://placement.bitmesra.ac.in/')
 
 elem1 = browser.find_element_by_css_selector('#txtUsername')
+elem2 = browser.find_element_by_css_selector('#txtPassword')
+elem3 = browser.find_element_by_css_selector('#imgSubmit')
 
 elem1.send_keys('Enter your login id/email here')
-
-#txtUsername
-
-elem2 = browser.find_element_by_css_selector('#txtPassword')
-
 elem2.send_keys('Enter your password here')
-
-#txtPassword
-
-
-elem3 = browser.find_element_by_css_selector('#imgSubmit')
 elem3.click()
